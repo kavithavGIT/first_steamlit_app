0a2c6a3e63a2331ed67cd8d4e48e757e9b6c4f4b
Kavitha-snowflake <115179690+kavithavGIT@users.noreply.github.com>
2022-10-07 16:44:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,26 +4,26 @@ import pandas
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado toast')
+     streamlit.title('My Mom\'s New Healthy Diner')
+     streamlit.header('Breakfast Favorites')
+     streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+     streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
+     streamlit.text('🐔Hard-Boiled Free-Range Egg')
+     streamlit.text('🥑🍞Avocado toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+     streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+    my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-  fruits_to_show = my_fruit_list.loc[fruits_selected]
+    fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+    fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 # Display the table on the page.
-   streamlit.dataframe(fruits_to_show)
+    streamlit.dataframe(fruits_to_show)
 
   #New section to display API response
     streamlit.header("Fruityvice Fruit Advice!")
