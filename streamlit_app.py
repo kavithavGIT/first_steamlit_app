aac1c165584972efc21b626bfdbe6b4352ce31da
Kavitha-snowflake <115179690+kavithavGIT@users.noreply.github.com>
2022-10-07 16:46:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ from urllib.error import URLError
 
   #New section to display API response
     streamlit.header("Fruityvice Fruit Advice!")
-    try:
+ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
      streamlit.error("Please select a fruit to get information.")
